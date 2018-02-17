cd19c29fd2cc9dcb2562c2f32e8f37ce9395caa4
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-16 18:39:52 -0700

    Create Login View component

---
+++ src/components/LoginView/LoginView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from '../Login/Login';
+import SignupContainer from '../Signup/SignupContainer';
+
+const LoginView = () => (
+  <div>
+    <Login />
+    <SignupContainer />
+  </div>
+);
+
+export default LoginView;
