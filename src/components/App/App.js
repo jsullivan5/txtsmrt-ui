d9082f448b4a42fab21119d1ac194ecf563e4467
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-19 10:05:19 -0700

    Refactor sub routing into app component from index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Header from '..//Header/Header';
+import Login from '..//Login/Login';
+import SignupContainer from '../Signup/SignupContainer';
 
-const App = () => (
-  <main>
-    <h1>This is the amazing app</h1>
-  </main>
-);
 
-export default App;
+class App extends Component {
+  componentDidUpdate(prevProps) {
+    // const { dispatch, redirectUrl } = this.props;
+    const isLoggingOut = prevProps.isLoggedIn && !this.props.isLoggedIn;
+    const isLoggingIn = !prevProps.isLoggedIn && this.props.isLoggedIn;
+
+    if (isLoggingIn) {
+      // dispatch(navigateTo(redirectUrl));
+    } else if (isLoggingOut) {
+      // Cleanup cookie on logout ect.
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignupContainer} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggedIn: state.loggedIn,
+    redirectUrl: state.redirectUrl,
+  };
+}
+
+export default connect(mapStateToProps, null)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import store, { history } from './store';
 import App from './components/App/App';
-import LoginView from './components/LoginView/LoginView';
-import Header from './components/Header/Header';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
@@ -14,13 +12,7 @@ import './index.css';
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/login" component={LoginView} />
-        </Switch>
-        <App />
-      </div>
+      <Route path="/" component={App} />
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
