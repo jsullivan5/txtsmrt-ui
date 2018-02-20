10a67a0066d34326d15c1017b09481cc3eb2f2cb
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 14:19:43 -0700

    Add getUser to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,6 +22,21 @@ export const userSignup = user => (
   }
 );
 
+export const getUser = () => (
+  (dispatch) => {
+    fetch(`${environment.apiUrl}/user/login`, {
+      method: 'GET',
+      credentials: 'include',
+    })
+      .then(response => response.json())
+      .then((user) => {
+        console.log(user);
+        dispatch(setUser(user));
+      })
+      .catch(error => console.error(error));
+  }
+);
+
 export const userLogin = user => (
   (dispatch) => {
     fetch(`${environment.apiUrl}/user/login`, {
@@ -48,6 +63,8 @@ export const userLogout = () => (
       .then((response) => {
         console.log(response);
         dispatch(setUser({ }));
+        document.cookie =
+          'connect.sid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
       })
       .catch(error => console.error(error));
   }
