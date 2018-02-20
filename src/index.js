efb51634eae0377818d5b4f0e5b5f7b00df99cac
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 14:20:55 -0700

    Render AppContainer in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { Route } from 'react-router-dom';
 import store, { history } from './store';
-import App from './components/App/App';
+import AppContainer from './components/App/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
@@ -12,7 +12,7 @@ import './index.css';
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Route path="/" component={App} />
+      <Route path="/" component={AppContainer} />
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root'),
