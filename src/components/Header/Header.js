4aa2ac824cf488efc2e117716542b47fd520dc3c
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 00:30:45 -0700

    Refactor Header to stateful component with conditional rendering login button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = () => (
-  <header className="header">
-    <div className="logo">
-      <h1>txtSmart</h1>
-    </div>
-    <nav>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
-      <NavLink to="/mytexts">My Texts</NavLink>
-      <NavLink to="/community">Community</NavLink>
-    </nav>
-  </header>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  async handleLogout() {
+    const { history, userLogout } = this.props;
+    await userLogout();
+    history.push('/');
+  }
+
+  renderLogin() {
+    return this.props.user.phoneNumber ? (
+      <button onClick={this.handleLogout}>
+        Logout
+      </button>
+    ) :
+      <NavLink to="/login">Login</NavLink>;
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <div className="logo">
+          <h1>txtSmart</h1>
+        </div>
+        <nav>
+          {this.renderLogin()}
+          <NavLink to="/mytexts">My Texts</NavLink>
+          <NavLink to="/community">Community</NavLink>
+        </nav>
+      </header>
+    );
+  }
+}
+
+Header.defaultProps = {
+  history: {},
+  userLogout: f => f,
+  user: {},
+};
+
+Header.propTypes = {
+  history: PropTypes.object,
+  userLogout: PropTypes.func,
+  user: PropTypes.object,
+};
 
 export default Header;
