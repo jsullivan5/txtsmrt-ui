cf6a8e1b7a0d3b752421b3ddf056736de0fe80d8
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 23:16:59 -0700

    Add messageReducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { userReducer } from './userReducer';
+import userReducer from './userReducer';
+import messageReducer from './messageReducer';
 
 export default combineReducers({
   router: routerReducer,
   user: userReducer,
+  messages: messageReducer,
 });
