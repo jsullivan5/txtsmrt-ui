b2406a677f661ccc83f264b647dc3a276d0d23cf
James Sullivan <jamesfsullivan5@gmail.com>
2018-02-20 23:17:17 -0700

    Refactor userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,12 +1,12 @@
 import * as constants from '../constants';
 
-export const userReducer = (state = {}, action) => {
+const userReducer = (state = {}, action) => {
   switch (action.type) {
     case constants.SET_USER:
-      return action.user;
+      return action.payload;
     default:
       return state;
   }
 };
 
-export const hello = 'hello';
+export default userReducer;
